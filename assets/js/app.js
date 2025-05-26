f9c721c114bc43cf2b6daf85b5db8c0aec2b5c52
Diego Garza <DiegoEGR4@gmail.com>
2025-05-26 11:23:06 -0600

    fix: arreglando el codigo de otra persona

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // Le faltaba el . antes de name
+const $b = document.querySelector('.blog'); // las clases se seleccionan con . no con #
+const $l = document.querySelector('.location'); // location no existe en el index
 
-function displayUser(username) {
+async function displayUser(username) { // No era una funcion asincrona entonces no se podia usar el await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json(); // No estabamos accediendo a los datos correctamente
+  console.log(data); // data no existia
+  $n.textContent = `${data.name}`; // data no existia y esta utilizando las comillas simples en lugar de backtick
+  $b.textContent = `${data.blog}`; // data no existia y esta utilizando las comillas simples en lugar de backtick
+  $l.textContent = `${data.location}`; // data no existia y esta utilizando las comillas simples en lugar de backtick
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // le faltaba el $ al nombre de la variable $n
 }
 
 displayUser('stolinski').catch(handleError);
